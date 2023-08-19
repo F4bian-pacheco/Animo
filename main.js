3ca8ed75dff53cb1e4fd667a6732901caa35444c
f4bian-pacheco <fabian.paxe@gmail.com>
2023-08-19 14:10:55 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+const loader = document.getElementById("loader");
+const page = document.getElementById("page-container");
+
+setTimeout(function () {
+  loader.style.display = "none";
+  page.style.visibility = "visible";
+}, 2000);
